e58a102400a343ba8e77cce763ab77551d611d8d
Tolga <kurosuisho@proton.me>
2024-12-01 15:52:45 +0100

    add endpoint for adding posts with unique ids

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,5 +1,6 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+import uuid
 
 app = Flask(__name__)
 CORS(app)  # This will enable CORS for all routes
@@ -14,6 +15,25 @@ POSTS = [
 def get_posts():
     return jsonify(POSTS)
 
+
+@app.route('/api/posts', methods=["POST"])
+def create_posts():
+    data = request.get_json(POSTS)
+    
+    if "title" not in data and "content" not in data:
+        return jsonify({"error": "Both 'title' and 'content' are required"}), 400
+    
+    new_id = uuid.uuid4().int
+    
+    new_post = {
+        "id": new_id,
+        "title": data["title"],
+        "content": data["content"]
+    }
+    
+    POSTS.append(new_post)
+    
+    return jsonify(new_post), 201
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5002, debug=True)
